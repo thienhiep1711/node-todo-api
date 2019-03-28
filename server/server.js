d34fb4587f281117f1fa0784cd5a32f495a4a6c4
Hiep Nguyen <thienhiep1711@users.noreply.github.com>
2019-03-28 17:24:37 +0700

    add Authenticate and middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -103,6 +105,31 @@ app.patch('/todos/:id', (req, res) => {
 
 })
 
+
+/* 
+USER MODEL
+*/
+
+// POST /user
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+    
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+})
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+})
+
 app.listen(port , () => {
   console.log(`Started on port ${port}`)
 });
@@ -144,5 +171,8 @@ app.listen(port , () => {
 // }, (e) => {
 //   console.log('Unable to save todo', e);
 // })
+
+
+
 
 module.exports = {app};
