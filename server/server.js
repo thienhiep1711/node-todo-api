3839d01d328a1904c428e412daf61195071cdf7b
Hiep Nguyen <thienhiep171193@gmail.com>
2020-04-11 17:21:34 +0700

    Allow CORS

--- server/server.js
+++ server/server.js
@@ -13,6 +13,14 @@ var {authenticate} = require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.post('/todos', authenticate, (req, res) => {
