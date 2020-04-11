2d13955b524ce1622046509a73b62f451c34ce79
Hiep Nguyen <thienhiep171193@gmail.com>
2020-04-11 22:46:38 +0700

    Update header token

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-auth");
   next();
 });
 
