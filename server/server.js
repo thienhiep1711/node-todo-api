e8dc7123e23eaa83c4f1f3957e6690eb49294023
Hiep Nguyen <thienhiep171193@gmail.com>
2020-04-11 18:09:14 +0700

    Sent token

--- server/server.js
+++ server/server.js
@@ -145,7 +145,7 @@ app.post('/users/login', (req, res) => {
 
   User.findByCredentials(body.email, body.password).then((user) => {
     return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
+      res.header('x-auth', token).send({user, 'auth':token});
     });
   }).catch((e) => {
     res.status(400).send();
